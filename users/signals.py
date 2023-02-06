33d6fcff02c011470848777828da3a838ec38284
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-02-05 20:54:27 -0500

    add signals for user and profile senders

---
+++ users/signals.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from users.models import Profile
+
+
+@receiver(signal=post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user, username=user.username, email=user.email, name=user.first_name
+        )
+
+
+@receiver(signal=post_delete, sender=Profile)
+def delete_user(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+
+# post_save.connect(receiver=create_profile, sender=Profile)
+# post_delete.connect(receiver=delete_user, sender=User)
