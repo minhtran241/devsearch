1d8d29a8aa765b9e52fd5785fee7461b90b75d03
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-02-15 09:27:46 -0500

    jwt, permission & projectVote api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,29 @@
 from typing import List
 from django.urls import path, URLPattern
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from api import views
 
 urlpatterns: List[URLPattern] = [
+    path(
+        route="users/token/",
+        view=TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        route="users/token/refresh/",
+        view=TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        route="users/token/verify/", view=TokenVerifyView.as_view(), name="token_verify"
+    ),
     path(route="", view=views.getRoutes),
     path(route="projects/", view=views.getProjects),
-    path(route="projects/<str:pk>", view=views.getProject),
+    path(route="projects/<str:pk>/", view=views.getProject),
+    path(route="projects/<str:pk>/vote/", view=views.projectVote),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from typing import List, Mapping
 from django.http import HttpRequest
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from api.serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(http_method_names=["GET"])
@@ -15,6 +16,7 @@ def getRoutes(request: HttpRequest) -> Response:
         {"POST": "/api/projects/id/vote"},
         {"POST": "/api/users/token"},
         {"POST": "/api/users/token/refresh"},
+        {"POST": "/api/users/token/verify"},
     ]
     return Response(data=routes)
 
@@ -31,3 +33,17 @@ def getProject(request: HttpRequest, pk: str) -> Response:
     project = Project.objects.get(id=pk)
     serializer = ProjectSerializer(instance=project, many=False)
     return Response(data=serializer.data)
+
+
+@api_view(http_method_names=["POST", "PUT"])
+@permission_classes(permission_classes=[IsAuthenticated])
+def projectVote(request: HttpRequest, pk: str) -> Response:
+    project = Project.objects.get(id=pk)
+    user_profile = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(owner=user_profile, project=project)
+    review.value = data["value"]
+    review.save()
+    project.get_vote_count
+    serializer = ProjectSerializer(instance=project, many=False)
+    return Response(serializer.data)
